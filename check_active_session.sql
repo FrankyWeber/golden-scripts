5e715bd5368d0a4f5db85a0cb250ebfab11c922a
Franky Weber Faust <weber08weber@gmail.com>
2017-05-18 06:57:48 -0300

    Update check_active_session.sql

--- check_active_session.sql
+++ check_active_session.sql
@@ -11,9 +11,9 @@ COL event FORMAT a10
 COL osuser FORMAT a8
 COL SERVICE_NAME FOR a15
 COL spid FORMAT a7
-COL SECONDS_IN_WAIT FORMAT 999999
+COL SECONDS_IN_WAIT FORMAT 9999
 COL inst_id FORMAT 99
-
+col logon_time format a10
   SELECT s.sid,
          s.serial#,
          p.spid,                                                
@@ -26,7 +26,7 @@ COL inst_id FORMAT 99
          s.SERVICE_NAME,
          w.event,
          s.SECONDS_IN_WAIT,
-         TO_CHAR (s.logon_time, 'YYYY-MM-DD HH24:MI:SS'), 
+         TO_CHAR (s.logon_time, 'YYYY-MM-DD HH24:MI:SS') logon_time, 
          /*s.sql_hash_value,*/
          s.sql_id,
          s.status
@@ -34,7 +34,7 @@ COL inst_id FORMAT 99
    WHERE s.sid = w.sid AND p.addr = s.paddr AND s.USERNAME IS NOT NULL
 --and upper(osuser) like '%VRX_FFAUST%'
 --and upper(s.machine) LIKE '%ORIGINAL%'
---and s.status = 'ACTIVE'
+and s.status = 'ACTIVE'
 --and w.wait_class<>'Idle'
 --and upper(w.event) like '%ENQ%'
 --and s.sid IN (434, 3419, 292)
@@ -42,4 +42,7 @@ COL inst_id FORMAT 99
 --and p.spid=21616
 --and s.program like '%???%'
 --and s.sql_id='67y9b1jstzt7k'
-ORDER BY TO_CHAR (s.logon_time, 'YYYY-MM-DD HH24:MI:SS') DESC;
+ORDER BY SECONDS_IN_WAIT DESC;
+--ORDER BY TO_CHAR (s.logon_time, 'YYYY-MM-DD HH24:MI:SS') DESC;
+
+
