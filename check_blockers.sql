95ab944df6a7b5311df584f155f026d6ed416951
Franky Weber Faust <weber08weber@gmail.com>
2017-05-19 19:52:33 -0300

    Create check_blockers.sql

---
+++ check_blockers.sql
@@ -0,0 +1,12 @@
+-- Criado por Franky Weber Faust 05/05/2013
+-- http://loredata.com.br
+-- Mostra sessões bloqueadas e bloqueadoras
+-- Extraído do livro "Oracle Database 11g Performance Tuning Recipes"
+select s1.username || '@' || s1.machine
+|| ' ( SID=' || s1.sid || ' ) is blocking '
+|| s2.username || '@' || s2.machine || ' ( SID=' || s2.sid || ' ) ' AS blocking_status
+from v$lock l1, v$session s1, v$lock l2, v$session s2
+where s1.sid=l1.sid and s2.sid=l2.sid
+and l1.BLOCK=1 and l2.request > 0
+and l1.id1 = l2.id1
+and l2.id2 = l2.id2 ;
