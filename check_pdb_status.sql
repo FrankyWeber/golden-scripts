b99b695ea3f2a5315776b6d38a94d185092affcf
Franky Weber Faust <weber08weber@gmail.com>
2017-06-13 21:31:22 -0300

    Create check_pdb_status.sql

---
+++ check_pdb_status.sql
@@ -0,0 +1,18 @@
+SELECT
+    p.name,
+    p.open_mode,
+    d.status,
+    TO_CHAR(
+        p.open_time,
+        'DD/MM/YYYY HH24:MI:SS'
+    ) open_time,
+    p.total_size / 1024 / 1024 total_mb,
+    p.max_size / 1024 / 1024 max_mb,
+    p.local_undo,
+    d.refresh_mode
+FROM
+    v$pdbs p,
+    dba_pdbs d
+WHERE
+    p.name = d.pdb_name
+ORDER BY 1;
