d52340307a8aa31744a426b83d51ce86f409a5d9
Franky Weber Faust <weber08weber@gmail.com>
2025-07-28 12:05:24 -0300

    Update cleanup_backups.sh
    Just editing some variables and comments.

--- cleanup_backups.sh
+++ cleanup_backups.sh
@@ -1,39 +1,53 @@
 #!/bin/bash
+# Franky Weber Faust -- July 2025 -- weber08weber@gmail.com -- https://loredata.com.br
+# Copyright (C) 2025 Franky Faust
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+# More info and git repo: https://github.com/FrankyWeber/golden-scripts
+#
+# The current script version is 20250728
+#
+# History :
+#
+# 20250728 - Franky Faust - Initial release with retention and extension variables to search and delete backups or any sort of files from specified directories.
+#
 
 # ==============================================================================
 #
-#          FILE:  cleanup_backups.sh
+# Script: cleanup_backups.sh
 #
-#         USAGE:  ./cleanup_backups.sh
+# Description: This script removes old backup files from specified directories based on a retention period.
 #
-#   DESCRIPTION:  This script removes old backup files from specified
-#                 directories based on a retention period.
-#
-#       OPTIONS:  ---
-#  REQUIREMENTS:  ---
-#          BUGS:  ---
-#         NOTES:  ---
-#        AUTHOR:  Your Name
-#  ORGANIZATION:
-#       CREATED:  07/28/2025 11:55
-#      REVISION:  1.0
-#
+# Usage: ./cleanup_backups.sh
 # ==============================================================================
 
-# --- Configuration ---
+# --- Configs ---
 
 # Set the number of days to keep the backup files.
 # Files older than this will be removed.
 RETENTION_DAYS=45
 
-# An array of directories to clean up.
+# A list of directories to clean up.
 # Add or remove directories as needed.
 TARGET_DIRECTORIES=("/zfs/backup1" "/zfs/backup2")
 
-# An array of file extensions to target for deletion.
+# A list of file extensions to target for deletion.
 # Use "*" to target all files regardless of extension.
-# Example: FILE_EXTENSIONS=("*.bak" "*.zip" "*.tar.gz")
-FILE_EXTENSIONS=("*.bak")
+# Example: FILE_EXTENSIONS=("*.bak" "*.zip" "*.tar.gz" "*.bkup" "*.bkp")
+FILE_EXTENSIONS=("*.bkup")
 
 # --- Script Logic ---
 
